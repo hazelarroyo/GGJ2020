f21908476a6b2ee3a7ef8726be10dd277d716e23
mollbin <mollbin@users.noreply.github.com>
2020-02-01 16:55:51 -0500

    intro

---
+++ Assets/_Scripts/AnimatedTextScriptB.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedTextScriptB : MonoBehaviour
+{
+    Text txt;
+    string story;
+
+    public Text prevText;
+    public Text nextText;
+
+    void Awake()
+    {
+        txt = GetComponent<Text>();
+        story = txt.text;
+        txt.text = "";
+
+        StartCoroutine("PlayText");
+    }
+
+    IEnumerator PlayText()
+    {
+        foreach (char c in story)
+        {
+            txt.text += c;
+            yield return new WaitForSeconds(0.08f);
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        nextText.gameObject.SetActive(true);
+        prevText.gameObject.SetActive(false);
+
+        yield return null;
+    }
+}
---
+++ Assets/_Scripts/IntroAnimationScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroAnimationScript : MonoBehaviour
+{
+    public Animator anim;
+
+    public Text lastText;
+
+    void Awake()
+    {
+        StartCoroutine("IntroAnims");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator IntroAnims()
+    {
+        yield return new WaitForSeconds(27);
+
+        anim.Play("castle");
+
+        yield return new WaitForSeconds(26);
+
+        anim.Play("shopkeep");
+
+        yield return new WaitForSeconds(30);
+
+        anim.Play("title");
+
+        yield return null;
+    }
+}
