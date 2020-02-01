83fa086e988914826cef136b92eb7977d1f24578
mitchellghl <46975869+mitchellghl@users.noreply.github.com>
2020-02-01 12:26:10 -0500

    Added basic loot system

---
+++ Assets/_Scripts/LootScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootScript : MonoBehaviour
+{
+    public GameObject[] loot;
+    public float currentLootTime = 0f;
+    public float maxLootTime = 3f;
+    public float increaseTimer = 0.1f;
+    public int numLoot;
+    bool looting;
+    bool collided = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && collided == false)
+        {
+            collided = true;
+            Debug.Log("Collided is true");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && collided == true)
+        {
+            collided = false;
+            Debug.Log("Collided is false");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) //Arbitrary set to A for testing
+        {
+            if (currentLootTime <= maxLootTime) //If timer under max, increase it
+            {
+                currentLootTime += increaseTimer;
+                Debug.Log(currentLootTime);
+            }
+            if (currentLootTime >= maxLootTime && looting == false) //If timer over max, spawn loot
+            {
+                GetLoot();
+                looting = true;
+            }
+        }
+    }
+
+    private void GetLoot() //Spawn loot
+    {
+        Debug.Log("I got here");
+        for (int i = 0; i < numLoot; i++) //Repeat for numLoot you want to spawn
+        {
+            int rand = Random.Range(0, loot.Length); //Pick a random game object in the array
+            Instantiate(loot[rand], transform.position, Quaternion.identity); //Spawn it
+            Debug.Log("Spawned " + i + "of " + numLoot);
+        }
+        Destroy(gameObject); //Get rid of the corpse
+    }
+}
