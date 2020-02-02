5e36edab97bfb9c79e6f9c3d8274ba5d41889787
mollbin <mollbin@users.noreply.github.com>
2020-02-02 11:14:20 -0500

    opening anims

---
+++ Assets/_Scripts/OpeningAnimScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningAnimScript : MonoBehaviour
+{
+    public Animator anim;
+
+    REALPlayerMove playerMove;
+
+    public GameObject player;
+
+    void Start()
+    {
+        playerMove = player.GetComponent<REALPlayerMove>();
+
+        StartCoroutine("OpeningAnims");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator OpeningAnims()
+    {
+        playerMove.enabled = false;
+
+        yield return null;
+    }
+}
