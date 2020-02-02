7f036819cb2e099209c654464057739055c7a63e
Hazel Arroyo <hazel.arroyo94@gmail.com>
2020-02-01 23:25:10 -0500

    Revert "uh"
    This reverts commit b3ae38e7861c50f1533b8c1c75361e24e7d9807e.

--- Assets/_Scripts/PlayerHealthScript.cs
+++ Assets/_Scripts/PlayerHealthScript.cs
@@ -5,33 +5,21 @@ using UnityEngine.UI;
 
 public class PlayerHealthScript : MonoBehaviour
 {
-    int playerHealth;
-
-    public Image heartA;
-    public Image halfHeartA;
-    public Image heartB;
-    public Image halfHeartB;
-    public Image heartC;
-    public Image halfHeartC;
-
     void Start()
     {
-        playerHealth = 6;
+        
     }
 
     void Update()
     {
-        if(playerHealth <= 5)
-        {
-            heartA.enabled = false;
-        }
+        
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.name == "Enemy")
+        if(collision.gameObject.CompareTag("Enemy"))
         {
-            playerHealth -= 1;
+
         }
     }
 }
