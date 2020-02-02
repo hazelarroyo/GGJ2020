c78b1ec3211f996f029d66acd6514f0b3a91a781
mitchellghl <46975869+mitchellghl@users.noreply.github.com>
2020-02-02 12:16:28 -0500

    this makes me feel better

---
+++ Assets/_Scripts/FEATURESTOADD.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FEATURESTOADD : MonoBehaviour
+{
+    ///Add score to UI
+    ///Update hearts when you take damage
+    ///Trigger death when you run out of life
+    ///Spawn a bat every x seconds (rather than using corpse spawns)
+    ///Increase treasure size
+    ///Add visual cue for which part goes with which
+    ///Add trigger by the shop to end level and get points
+    ///Update item UI for how many of each you've created
+}
