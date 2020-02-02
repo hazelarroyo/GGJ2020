03ac7bc40b6690191415a2c329a6e072d0e26a0a
Hazel Arroyo <hazel.arroyo94@gmail.com>
2020-02-01 22:46:46 -0500

    Revert "Revert "shit shit shit""
    This reverts commit 2050bef33ec928981172531457f8f15978f557b3.

---
+++ Assets/_Scripts/PlayerHealthScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+
+        }
+    }
+}
